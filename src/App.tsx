7d38c67a9e39a613a2f7dec5c85507c3f846c2c5
tomirodriguez <tomirodriguez.89@gmail.com>
2022-06-21 19:42:03 +0200

    add basic SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Helmet } from 'react-helmet';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="h-screen bg-blue-300">
+      <Helmet>
+        <meta lang="en" />
+        <meta charSet="utf-8" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>WebTwitter</title>
+        <meta name="description" content="Challenge para Lookiero" />
+      </Helmet>
+      <header></header>
     </div>
   );
 }
