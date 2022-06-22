0d03550c8da76b92b93f50ab5e2b0ed7760032d0
tomirodriguez <tomirodriguez.89@gmail.com>
2022-06-22 12:22:44 +0200

    config: add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#1D9BF0',
+        'hover-blue': '#1D9BF01A',
+        'primary-white': '#E7E9EA',
+        'hover-white': '#E7E9EA1A',
+      },
+    },
   },
   plugins: [],
 };
