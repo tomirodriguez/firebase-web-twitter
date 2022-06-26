2d30d3a7e00418af2b363de56de4698aafe89cd9
tomirodriguez <tomirodriguez.89@gmail.com>
2022-06-26 09:14:10 +0200

    feat(PostTweet): ui for tweet posting

---
+++ src/components/ui/PostTweet.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useState } from 'react';
+import { User } from '../../icons/User';
+import { PrimaryButton } from './PrimaryButton';
+
+export const PostTweet = () => {
+  const [tweet, setTweet] = useState('');
+  const isTweetValid = tweet.length <= 280 && tweet.length > 0;
+
+  const handleTweetChange = (event: ChangeEvent<HTMLDivElement>) => {
+    setTweet(event.target.innerText);
+  };
+
+  return (
+    <div>
+      <div className="flex items-start">
+        <div className="mr-4 shrink-0">
+          <User size={48} />
+        </div>
+        <div className="grow relative">
+          <div
+            className="w-full grow-0 break-words outline-none text-xl pt-2"
+            contentEditable
+            style={{
+              overflow: 'wrap',
+              userSelect: 'text',
+              overflowWrap: 'anywhere',
+            }}
+            onInput={handleTweetChange}
+            aria-errormessage="error-message"
+            aria-invalid={!isTweetValid}
+          />
+          {tweet.length === 0 && (
+            <span className="absolute text-xl top-0 pt-2 text-secondary-text">
+              What's happening?
+            </span>
+          )}
+        </div>
+      </div>
+      <div className="flex justify-end items-center">
+        {tweet.length > 280 && (
+          <p id="error-message" className="text-error text-sm mr-3">
+            {280 - tweet.length}
+          </p>
+        )}
+        <div className="w-20 h-9 text-sm">
+          <PrimaryButton title="Tweet" text="Tweet" disabled={!isTweetValid} />
+        </div>
+      </div>
+    </div>
+  );
+};
