782a451a9443969d5abe278959fba5b4c5ffdb80
tomirodriguez <tomirodriguez.89@gmail.com>
2022-06-26 23:28:47 +0200

    feat(UserProfilePic): creates user profile pic from user data

---
+++ src/components/ui/UserProfilePic.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import { User } from '../../icons';
+
+type Props = {
+  src: string | undefined;
+  name: string;
+  size?: number;
+};
+
+export const UserProfilePic: FC<Props> = ({ src, name, size = 48 }) => {
+  return src ? (
+    <img
+      className="rounded-full"
+      src={src}
+      alt={name}
+      width={size}
+      height={size}
+    />
+  ) : (
+    <User size={size} />
+  );
+};
