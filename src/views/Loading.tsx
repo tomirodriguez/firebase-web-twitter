33bea77e35162b110c28b938b6345f0f869f285e
tomirodriguez <tomirodriguez.89@gmail.com>
2022-06-27 10:50:51 +0200

    feat(Loading): add loading view

---
+++ src/views/Loading.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import Icons, { iLogo } from '../icons';
+import { PRIMARY_BLUE } from '../colors/index';
+
+export const Loading: FC = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-black">
+      <Icons icon={iLogo} size={100} color={PRIMARY_BLUE} />
+    </div>
+  );
+};
